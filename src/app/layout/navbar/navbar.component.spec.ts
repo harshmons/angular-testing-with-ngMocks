62933dcd881f774f3f91289e2ec22c3486409140
Harsh Verma <harshverma04111989@gmail.com>
2024-03-06 16:25:34 +0530

    Added some more test cases

--- src/app/layout/navbar/navbar.component.spec.ts
+++ src/app/layout/navbar/navbar.component.spec.ts
@@ -1,15 +1,25 @@
-import { AppModule } from './../../app.module';
 import { NavbarComponent } from './navbar.component';
-import { MockBuilder, MockRender } from 'ng-mocks';
-import { of } from 'rxjs';
-import { cold } from 'jest-marbles';
+import { MockBuilder, MockRender, ngMocks } from 'ng-mocks';
+import { Subject, of } from 'rxjs';
+import { LayoutModule } from '../layout.module';
+import { Store } from '@ngrx/store';
+import { mockShoppingCart } from '../../mocks';
+import { MatBadge } from '@angular/material/badge';
+import { cold, hot } from 'jest-marbles';
 
-describe('Navbar component', () => {
+describe('Navbar component One way', () => {
+  // In this we are asserting the count on the rendered HTML
+  
+  let mockStore$ : Subject<any>;
   beforeEach(() => {
-    return MockBuilder(NavbarComponent, AppModule)
-    // .mock(ShoppingCartService, {
-    //   getCartCount$: () => of(1),
-    // });
+    mockStore$ = new Subject();
+    jest.clearAllMocks();
+  })
+
+  beforeEach(() => {
+    return MockBuilder(NavbarComponent, LayoutModule).provide({provide:Store,useValue:{
+      select: jest.fn().mockReturnValue(mockStore$),
+    }})
   });
 
   it('should be defined', () => {
@@ -17,10 +27,32 @@ describe('Navbar component', () => {
     expect(fixture.componentInstance).toBeDefined();
   });
 
-  // it('should get the count', () => {
-  //   const fixture = MockRender(NavbarComponent);
-  //   const component = fixture.componentInstance;
-  //   const expected$ = cold('(a|)', { a: 1 });
-  //   expect(component.count$).toBeObservable(expected$);
-  // });
+  it('should render the correct cart count in mat-icon', async () => {
+    const fixture = MockRender(NavbarComponent);
+    mockStore$.next(mockShoppingCart.length)
+    const matBadge = ngMocks.findInstance(fixture,MatBadge)
+    fixture.detectChanges();
+    expect(matBadge.content).toBe(mockShoppingCart.length);
+  });
+
+});
+describe('Navbar component Other Way', () => {
+// In this we are asserting only the count observable through jest marbel
+  beforeEach(() => {
+    return MockBuilder(NavbarComponent, LayoutModule).provide({provide:Store,useValue:{
+      select: jest.fn().mockReturnValue(hot('-a|',{a:mockShoppingCart.length}))
+    }})
+  });
+
+  it('should be defined', () => {
+    const fixture = MockRender(NavbarComponent);
+    expect(fixture.componentInstance).toBeDefined();
+  });
+
+  
+  it('should have the correct data in count$', async () => {
+    const fixture = MockRender(NavbarComponent);
+    const expected = cold('-a|',{a:mockShoppingCart.length});
+    expect(fixture.componentInstance.count$).toBeObservable(expected);
+  });
 });
