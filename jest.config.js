34c21b7da920d77aef44fc1abf2902f8f81c2340
Harsh Verma <harshverma04111989@gmail.com>
2024-04-05 16:10:26 +0530

    ingnored some files

--- jest.config.js
+++ jest.config.js
@@ -20,6 +20,7 @@ module.exports = {
   },
   collectCoverage:true,
   "collectCoverageFrom": [
+    "!src/**/*.ts",
     "src/**/*.service.ts",
     "src/**/*.component.ts",
     "src/**/*.directive.ts",
